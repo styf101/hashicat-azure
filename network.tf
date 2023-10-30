c6ead398d82efce149150b7b0218d40f03fc99d6
Your Name <you@example.com>
2023-10-30 20:01:01 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/TestLab101/network/azurerm"
   version = "3.5.0"
-resource_group_name = "myresourcegroup"
+resource_group_name = "${var.prefix}-workshop"
   # insert required variables here
 }
